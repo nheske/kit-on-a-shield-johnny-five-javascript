903dcfc3c05974f2c08ad0fa02765929b6f66f39
nheske <32206709+nheske@users.noreply.github.com>
2019-02-22 16:08:07 -0800

    Create temperature.js

---
+++ temperature.js
@@ -0,0 +1,24 @@
+//Analog 04 = LM34 Temperature Sensor / I2C SDA
+const five = require("johnny-five");
+const arduino = new five.Board();
+
+arduino.on("ready", function () {
+
+    // Access the temperature sensor on pin A0
+    const thermometer = new five.Thermometer({
+        controller: "LM34",
+        pin: "A4",
+        freq: 1000
+    });
+
+    // Data event listener with callback function
+    // Will capture incoming sensor readings
+    thermometer.on("data", function () {
+
+        // Callback has readings in celsius, fahrenheit and kelvin
+        console.log(this.celsius + "°C");
+        console.log(this.fahrenheit + "°F");
+        console.log(this.kelvin + "°K");
+        console.log("========");
+    });
+});
