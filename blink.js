3e6e8368d8f14b742028b5599b6f4c00a99de5f6
nheske <32206709+nheske@users.noreply.github.com>
2019-02-22 16:36:18 -0800

    Create blink.js

---
+++ blink.js
@@ -0,0 +1,22 @@
+/* 
+ You have the following variables available to your script:
+
+ io  = the firmata instance for the connected hardware
+ browserbots  = namespace of APIs and controls to use in your script
+ require  = subset of the NodeJS require with bundled robotics, IoT, and utility libs 
+*/ 
+
+var five = require('johnny-five');
+
+var board = new five.Board();
+
+board.on('ready', function(){
+  // Default to pin 13
+  var led = new five.Led(13);
+  led.blink(2000);
+  
+});
+
+board.on('error', function(err){
+  browserbots.speak('HEY! ' + err);
+});
